1ca4c8091f80344b85007df71d1398dd6a43c786
wxxk <dwde22@gmail.com>
2023-07-14 14:18:41 +0900

    templates

--- SpringMiniPrj3th/src/main/java/com/example/myapp/HelloController.java
+++ SpringMiniPrj3th/src/main/java/com/example/myapp/HelloController.java
@@ -1,16 +1,15 @@
 package com.example.myapp;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@Mapper
 public class HelloController {
 	@RequestMapping("/")
-	public @ResponseBody String index() {
-		return "welcome home";
+	public String index() {
+		return "index";
 	}
 }
 
