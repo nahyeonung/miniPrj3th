35c2f637f81297b45086ef1ed84eaecfc4feb247
wxxk <dwde22@gmail.com>
2023-07-25 14:16:23 +0900

    2023-07-25 장바구니/구매내역 프론트

--- SpringMiniPrj3th/src/main/java/com/example/myapp/cart/controller/CartController.java
+++ SpringMiniPrj3th/src/main/java/com/example/myapp/cart/controller/CartController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -40,6 +39,7 @@ public class CartController {
 	public String insertCart(Cart cart, RedirectAttributes redirectAttrs, HttpSession session) {
 		String userId = (String)session.getAttribute("userId");
 		if(userId != null && !userId.equals("")) {
+			System.out.println(cart.getCartCnt());
 			cartService.insertCart(cart);
 			return "redirect:/cart";
 		}else {
